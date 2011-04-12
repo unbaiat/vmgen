737dda5856e8cfcf01f6cf39545d6c447218feaf
revan <umirc3a@gmail.com>
2011-04-13 01:19:36 +0300

    added key login to ssh

--- conf-parser/runCommands.py
+++ conf-parser/runCommands.py
@@ -3,13 +3,14 @@ import shlex
 
 def executeCommand(command):
 	args = shlex.split(command)
+	print "Execute: ", args
 	p = Popen(args, stdout=PIPE)
 	s = p.communicate()
 	print p.returncode
 	return (p.returncode, s[0])
 
 def executeCommandSSH(command):
-	return executeCommand("ssh " + user + "@" + host + " " + command)
+	return executeCommand("ssh " + key + " " + user_host + " " + command)
 
-user = "root"
-host = "vmaster"
+user_host = "root@vmaster"
+key = "-i vmaster_key.private"
