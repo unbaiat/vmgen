45c1950199bd17b531f80d0b55f61d5b8d8df254
Alex Andrei <alex.f.andrei@gmail.com>
2011-06-02 02:03:28 +0300

    - updated Openvz module: hardware & user config, logging

--- conf-parser/vmgCommanderOpenvz.py
+++ conf-parser/vmgCommanderOpenvz.py
@@ -1,26 +1,36 @@
 from vmgCommanderBase import CommanderBase
 from vmgInstallerDummy import InstallerDummy
 from runCommands import *
+from vmgLogging import *
+from vmgControlVmware import *
 import shutil
 import os
 import time
 
+vm="C:\Users\MickeyMouse\Documents\Virtual Machines\Fedora32-OpenVz\Fedora32-OpenVz.vmx"
+host="root@fedora-openvz"
+
+log = logging.getLogger("vmgen.vmgCommanderOpenvz")
+
 class CommanderOpenvz(CommanderBase):
 	def startVM(self):
-		print "\nStarting the VM..."
 		try:
-                        start_cmd = "vzctl start " + section.get("vm_id")
-                        executeCommand(start_cmd)
+                        # create container
+                        vm_id = self.data.getSection("hardware").get("vm_id")
+                        log.debug("Start container ...")
+                        executeCommandSSH("vzctl start " + vm_id)
                 except Exception:
-                        print "Cannot start container"
+                        log.error("Cannot start container ...")
 
 	def shutdownVM(self):
-		print "\nShutting down the VM..."
 		try:
-                        stop_cmd = "vzctl stop " + section.get("vm_id")
-                        executeCommand(stop_cmd)
-                except Exception:
-                        print "Cannot stop container"
+                        vm_id = self.data.getSection("hardware").get("vm_id")
+                        log.debug("Stop container ...")
+                        executeCommandSSH("vzctl stop " + vm_id)
+                        log.debug("Destroy container ...")
+                        executeCommandSSH("vzctl destroy " + vm_id)
+                except Exception as exc:
+                        log.error("Cannot stop container: " + str(exc))
 
 	def connectToVM(self):
 		print "\nEstablishing connection to the VM..."
@@ -29,28 +39,37 @@ class CommanderOpenvz(CommanderBase):
 		print "\nTerminating connection to the VM..."
 
 	def setupHardware(self):
-		print "\nCreating the hardware configuration..."
-		section = self.data.getSection("hardware")
-
+		log.debug("Creating the hardware configuration...")
                 try:
-                        # create container
+                        section = self.data.getSection("hardware")
                         vm_id = section.get("vm_id")
-                        create_cmd = "vzctl create " + vm_id
-                        create_cmd += " --ostemplate " + section.get("os")
-                        executeCommand(create_cmd)
-                        # set container parameters
                         setup_cmd = "vzctl set " + vm_id
-                        # cpu numbers
-                        executeCommand(setup_cmd + " --numproc " + section.get("num_cpu"))
+
+                        # set the user and host used for the SSH connection
+                        setUserHost(host)
+
+                        # power on VMware machine
+                        log.debug("\tStarting the virtual machine...")
+                        try_power_on_vm(vm)
+
+                        # create container
+                        log.debug("Create container ...")
+                        executeCommandSSH("vzctl create " + vm_id + " --ostemplate " + section.get("os"))
+                        
+                        # cpu number
+                        if section.contains("num_cpu"):
+                                executeCommandSSH(setup_cmd + " --numproc " + section.get("num_cpu"))
+                                
                         # memory size
-                        # TODO: recheck this
-                        executeCommand(setup_cmd + " --vmguargpages " + section.get("ram"))
+                        if section.contains("ram"):
+                                executeCommandSSH(setup_cmd + " --vmguargpages " + section.get("ram"))
+                                
                         # harddisk size: get the size of the first entry in hdd list
-                        hdd = section.get("hdds").values[0]
-                        executeCommand(setup_cmd + " --diskspace " + hdd.get("size"))
-                        # interfaces
-                except Exception:
-                        print "Cannot setup hardware"
+                        executeCommandSSH(setup_cmd + " --diskspace " + section.get("hdds").values[0].get("size"))
+                        
+                        # TODO: interfaces
+                except Exception as exc:
+                        log.error("Cannot complete hardware configuration: " + str(exc))
 		
 	def setupConfigurations(self):
 		print "\nConfiguring system settings..."
@@ -61,35 +80,55 @@ class CommanderOpenvz(CommanderBase):
 	def setupNetwork(self):	
 		print "\nSetting up the network configurations..."
 		section = self.data.getSection("network")
-		for k, v in section.items():
-			print k, "=", v
+		#for k, v in section.items():
+		#	print k, "=", v
 
 	def setupUsers(self):
-		print "\nAdding users..."
+		if self.data.contains("users") is False:
+                        log.debug("No users to add ...")
+                        return
+                log.debug("Adding users ...")
 		section = self.data.getSection("users")
+		vm_id = self.data.getSection("hardware").get("vm_id")
+		
 		for i, u in enumerate(section.get("users")):
-			print "Add user #"+i
-			print "\tName: ", u["name"]
-			print "\tPassword: ", u["passwd"]
-			print "\tGroup: ", u["group"]
-			print "\tHome directory: ", u["home_dir"]
-			print "\tPermissions: ", u["perm"]
-                        adduser = "vzctl set " + section.get("vm_id")
-                        adduser += " --userpasswd " + u["name"] + ":" + u["passwd"]
-                        executeCommand(adduser)
+                        if u.contains('name') is False:
+                                log.error("Cannot add user #" + str(i) + ": missing name")
+                                continue
+                        log.debug("Add user #" + str(i) + ": " + u['name'])
+
+                        # User name & password
+                        if u.contains('name') is False:
+                                log.error("Cannot add user #" + str(i) + ": missing password")
+                        executeCommandSSH("vzctl set " + vm_id + " --userpasswd " + u["name"] + ":" + u["passwd"])
+
+                        # User group
+                        if u.contains('group'):
+                                # make sure the group exists
+                                executeCommandSSH("vzctl exec groupadd " + u['group'])
+                                # set user group
+                                executeCommandSSH("vzctl exec usermod -g " + u['group'] + " " + u['name'])
+
+                        # User home directory
+                        if u.contains('home_dir'):
+                                executeCommandSSH("vzctl exec usermod -d " + u['home_dir'] + " " + u['name'])
+
+                        # TODO: User permissions
+                        if u.contains('perm'):
+                                print "\tPermissions: ", u["perm"]
 	
 	def setupServices(self):
 		print "\nInstalling services..."
 		section = self.data.getSection("services")
-		self.installPrograms(section)
+		#self.installPrograms(section)
 
 	def setupDeveloperTools(self):
 		print "\nInstalling developer tools..."
 		section = self.data.getSection("devel")
-		self.installPrograms(section)
+		#self.installPrograms(section)
 
 	def setupGuiTools(self):
 		print "\nInstalling GUI tools..."
 		section = self.data.getSection("gui")
-		self.installPrograms(section)
+		#self.installPrograms(section)
 	
